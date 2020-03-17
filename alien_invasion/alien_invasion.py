50bc1e860f928e09db51faf57a399e1c1e0b035d
Kunmeng Wang <wangkunmeng@nextjoy.com>
2020-03-17 18:54:24 +0800

    练习：12章结束

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,29 +3,31 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
 
     pygame.init()
-    ai_setting = Settings()
-    screen = pygame.display.set_mode((ai_setting.screen_with, ai_setting.screen_height))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_with, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+    # 创建一个子弹的编组
+    bullets = Group()
 
     # 开始游戏的主循环
     while True:
-
-        # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # 每次循环都要重新绘制屏幕
-        screen.fill(ai_setting.bg_color)
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,3 +8,13 @@ class Settings:
         self.screen_with = 1200
         self.screen_height = 800
         self.bg_color = 230, 230, 230
+
+        # 飞船的设置
+        self.ship_speed_factor = 2
+
+        # 子弹的设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
