f0822e09488cb8529a58ecf031adcb44dcd756bd
Kunmeng Wang <wangkunmeng@nextjoy.com>
2020-03-23 18:59:14 +0800

    chapter-13：结束

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from game_stats import GameStats
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -13,21 +14,25 @@ def run_game():
 
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_with, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
-
-    # 创建一艘飞船
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+    # 创建一艘飞船、 创建一个子弹的编组、创建一个外星人编组
     ship = Ship(ai_settings, screen)
-    # 创建一个子弹的编组
     bullets = Group()
-
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        # if外为了能死亡后保持刷新屏幕
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -5,12 +5,14 @@ class Settings:
         """初始化游戏的设置"""
 
         # 屏幕的设置
-        self.screen_with = 1200
+        self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = 230, 230, 230
 
         # 飞船的设置
-        self.ship_speed_factor = 2
+        self.ship_speed_factor = 1
+        # 外星人的设置
+        self.alien_speed_factor = 1
 
         # 子弹的设置
         self.bullet_speed_factor = 1
@@ -18,3 +20,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
+        # 外星人群的设置
+        self.fleet_drop_speed = 30
+        # fleet_direction of 1 represents right, -1 represents left.
+        self.fleet_direction = 1
+        self.ship_limit = 3
